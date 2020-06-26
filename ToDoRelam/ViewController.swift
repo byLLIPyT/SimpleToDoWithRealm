1e8ea4c036e7feef65c172a6d903ea90f619a229
byLLIPyT <byllipyt@icloud.com>
2020-06-26 22:08:07 +0300

    Initial Commit

---
+++ ToDoRelam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoRelam
+//
+//  Created by Александр Уткин on 26.06.2020.
+//  Copyright © 2020 Александр Уткин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
